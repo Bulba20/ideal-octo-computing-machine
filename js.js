98bbb76cf0300e684d507f872c13965fcabc8dab
Bulba20 <alexander.2028@student.cahayabangsa.org>
2025-09-09 19:49:05 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+function test(){
+    alert("test")
+    document.getElementsById("main-heading").style.color = "fart"
+}
+
+function test2(){
+     document.getElementById("main-heading").textContent = "new text"
+}
